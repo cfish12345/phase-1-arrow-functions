da969d0836f3391157e25d314c8854396abcd0f5
christian fish <cfishmusic@hotmail.com>
2022-04-06 20:29:13 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+function divide() {
+    return 2000 / 100;
+}
+
+const square = parameter1 => parameter1 * parameter1;
+square(10);
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add(10,4);
